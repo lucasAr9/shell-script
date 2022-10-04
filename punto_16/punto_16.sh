8da82e853ca765fa692d7b744ea688097a1d9c68
Lucas Arrigoni <lukasarri999111@gmail.com>
2022-10-04 17:32:37 -0300

    punto 16

---
+++ punto_16/punto_16.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+# 16. Realizar un script que reciba como parámetro una extensión y haga un reporte con 2
+# columnas, el nombre de usuario y la cantidad de archivos que posee con esa extensión. Se
+# debe guardar el resultado en un archivo llamado reporte.txt.
+
+echo "$(ls /home)"
+
