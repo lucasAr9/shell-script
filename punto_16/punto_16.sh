de5634ac4b7d8350d75da4d200eec75d3e14a535
Lucas Arrigoni <lukasarri999111@gmail.com>
2022-10-05 18:41:11 -0300

    punto 18

--- punto_16/punto_16.sh
+++ punto_16/punto_16.sh
@@ -5,5 +5,18 @@
 # columnas, el nombre de usuario y la cantidad de archivos que posee con esa extensión. Se
 # debe guardar el resultado en un archivo llamado reporte.txt.
 
-echo "$(ls /home)"
 
+# con un solo usuario
+if [ $# -ne 1 ]; then
+    echo "El script necesita recibir una extencion de archvo como parametro."
+    exit 1
+fi
+
+usuario=$(whoami)
+extencion=".$1"
+cant="$(find "$HOME"/Documentos -name "*.$1" | wc -l)"
+
+echo "$usuario    $cant    $extencion" >> reporte.txt
+
+
+# con todos los usuarios
---
+++ punto_18.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# 18. Crear un script que verifique cada 10 segundos si un usuario se ha logueado en el sistema
+# (el nombre del usuario será pasado por parámetro). Cuando el usuario finalmente se loguee,
+# el programa deberá mostrar el mensaje ”Usuario XXX logueado en el sistema” y salir.
+
