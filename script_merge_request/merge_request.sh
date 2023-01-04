9070677aad0d363953c24201f450c4f3b46b398a
Lucas Arrigoni <lukasarri999111@gmail.com>
2023-01-04 18:39:06 -0300

    script merge request

--- script_merge_request/merge_request.sh
+++ script_merge_request/merge_request.sh
@@ -5,10 +5,7 @@
 # $3 = id del merge request
 # Ingresar a gitlab y generar un merge request
 if [ $# -ne 3 ]; then
-    echo "El script debe recibir como parametro:"
-    echo "El parametro 1 es el token de acceso."
-    echo "El parametro 2 es el ID de la aplicacion."
-    echo "El parametro 3 es el ID del merge request."
+    echo "Parametros incorrectos."
     exit 1
 fi
 
@@ -16,23 +13,49 @@ fecha_ejecucion=$(date +"%Y%m%d_%H%M%S")
 echo "FECHA Y HORA DE EJECUCION: " $fecha_ejecucion
 echo " "
 
-id=$(curl -v --location --request GET -H "PRIVATE-TOKEN: $1" -H "Accept: application/json, text/plain, /" -H "Content-Type: application/json" "http://gitserver.ggti.arba.gov.ar/api/v4/projects?search=$2" | cut -d":" -f2 | cut -d"," -f1)
+respuesta=$(curl -v --request GET -H "PRIVATE-TOKEN: $1" -H "Accept: application/json, text/plain, /" -H "Content-Type: application/json" "http://gitserver.ggti.arba.gov.ar/api/v4/projects?search=$2")
+echo $respuesta
+
+echo $respuesta | grep -o '"message":"401 Unauthorized"'
+if [ $? == 0 ]; then
+    echo "El token es incorrecto"
+    exit 2
+else
+    length=$(expr length "$respuesta")
+    if [ $length == 2 ]; then
+        echo "El nombre del repositorio es incorrecto."
+        exit 3
+    else
+        id=$(echo $respuesta | cut -d":" -f2 | cut -d"," -f1)
+    fi
+fi
 
 codigo=$(curl -v --location --request PUT -H "PRIVATE-TOKEN: $1" -H "Accept: application/json, text/plain, /" -H "Content-Type: application/json" "http://gitserver.ggti.arba.gov.ar/api/v4/projects/$id/merge_requests/$3/merge")
-echo $codigo
 
-# El mensaje
+echo $codigo | grep -o '"state":"merged"'
+if [ $? == 0 ]; then
+    echo "todo ok"
+    exit 0
+else
+    echo "El id del merge es incorrecto"
+    exit 4
+fi
+
+# exit 1
+# Parametros incorrectos.
+
+# exit 2
 # {"message":"401 Unauthorized"}
 # Significa que el token esta mal o vencio.
 
-# El mensaje
+# exit 3
 # {"message": "404" proyect not found}
 # Significa que el proyecto no se encontro.
 
-# El mensaje
+# exit 4
 # {"message":"405 Method Not Allowed"}
 # Significa que el id del merge request esta mal o no existe.
 
-# El mensaje
+# codigo 422
 # {"message":"422 Branch cannot be merged"}
 # Significa que no se pudo mergear.
